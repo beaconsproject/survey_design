edf531754d65e1cff72d8da3e013b4a16b73451a
Pierre R Vernier <pierre.vernier@gmail.com>
2023-01-26 16:53:30 -0800

    minor revisions to clustering script

--- non_app_clustering_script.R
+++ non_app_clustering_script.R
@@ -1,3 +1,6 @@
+library(sf)
+library(dplyr)
+
 factors = st_read("www/wolverines.gpkg", "survey_factors", quiet=T)
 
 # value to split between low/medium and high disturbance
@@ -13,7 +16,7 @@ factors[is.na(factors$merge100_pct),]$merge100_pct <- 0
 
 ## Split factors into 1) merge100_pct == 0, 2) 0 < merge100_pct <= 30, 
 ## 3) merge100_pct > 30
-factors %<>% 
+factors <- factors %>% 
   mutate(cluster = ifelse(test = merge100_pct == 0, yes = 1,
                           no = ifelse(test = merge100_pct > 0 & 
                                         merge100_pct <= split, 
@@ -52,55 +55,60 @@ ks_output <-
 
 ## Compare similarity of each randomly selected set to the other 2 clusters 
 ## i.e. Compare merge100_pct == 0 to merge100_pct > 0 & <= 30 and merge100_pct > 30
-simple12 <- simple %>%
-  filter(cluster %in% c(1,2))
-simple23 <- simple %>% 
-  filter(cluster %in% c(2,3))
-simple13 <- simple %>% 
-  filter(cluster %in% c(1,3))
+#simple12 <- simple %>%
+#  filter(cluster %in% c(1,2))
+#simple23 <- simple %>% 
+#  filter(cluster %in% c(2,3))
+#simple13 <- simple %>% 
+#  filter(cluster %in% c(1,3))
 
 ks_output_clust1 <- 
   tibble(
-    merge100_pct = ks.test(simple23$merge100_pct, simple1$merge100_pct)[[1]],
-    #merge500_pct = ks.test(simple23$merge500_pct, simple1$merge500_pct)[[1]],
-    placer_pct = ks.test(simple23$placer_pct, simple1$placer_pct)[[1]],
-    quartz_pct = ks.test(simple23$quartz_pct, simple1$quartz_pct)[[1]],
-    recent_fires_pct = ks.test(simple23$recent_fires_pct, simple1$recent_fires_pct)[[1]],
-    benchmark_pct = ks.test(simple23$benchmark_pct, simple1$benchmark_pct)[[1]],
-    elev_median = ks.test(simple23$elev_median, simple1$elev_median)[[1]],
-    elev_sd = ks.test(simple23$elev_sd, simple1$elev_sd)[[1]],
-    forest_pct = ks.test(simple23$forest_pct, simple1$forest_pct)[[1]],
-    wetland_pct = ks.test(simple23$wetland_pct, simple1$wetland_pct)[[1]],
-    water_pct = ks.test(simple23$water_pct, simple1$water_pct)[[1]]
+    merge100_pct = ks.test(factors$merge100_pct, simple1$merge100_pct)[[1]],
+    #merge500_pct = ks.test(factors$merge500_pct, simple1$merge500_pct)[[1]],
+    placer_pct = ks.test(factors$placer_pct, simple1$placer_pct)[[1]],
+    quartz_pct = ks.test(factors$quartz_pct, simple1$quartz_pct)[[1]],
+    recent_fires_pct = ks.test(factors$recent_fires_pct, simple1$recent_fires_pct)[[1]],
+    benchmark_pct = ks.test(factors$benchmark_pct, simple1$benchmark_pct)[[1]],
+    elev_median = ks.test(factors$elev_median, simple1$elev_median)[[1]],
+    elev_sd = ks.test(factors$elev_sd, simple1$elev_sd)[[1]],
+    forest_pct = ks.test(factors$forest_pct, simple1$forest_pct)[[1]],
+    wetland_pct = ks.test(factors$wetland_pct, simple1$wetland_pct)[[1]],
+    water_pct = ks.test(factors$water_pct, simple1$water_pct)[[1]]
   )
 
 ks_output_clust2 <- 
   tibble(
-    merge100_pct = ks.test(simple13$merge100_pct, simple2$merge100_pct)[[1]],
-    #merge500_pct = ks.test(simple13$merge500_pct, simple2$merge500_pct)[[1]],
-    placer_pct = ks.test(simple13$placer_pct, simple2$placer_pct)[[1]],
-    quartz_pct = ks.test(simple13$quartz_pct, simple2$quartz_pct)[[1]],
-    recent_fires_pct = ks.test(simple13$recent_fires_pct, simple2$recent_fires_pct)[[1]],
-    benchmark_pct = ks.test(simple13$benchmark_pct, simple2$benchmark_pct)[[1]],
-    elev_median = ks.test(simple13$elev_median, simple2$elev_median)[[1]],
-    elev_sd = ks.test(simple13$elev_sd, simple2$elev_sd)[[1]],
-    forest_pct = ks.test(simple13$forest_pct, simple2$forest_pct)[[1]],
-    wetland_pct = ks.test(simple13$wetland_pct, simple2$wetland_pct)[[1]],
-    water_pct = ks.test(simple13$water_pct, simple2$water_pct)[[1]]
+    merge100_pct = ks.test(factors$merge100_pct, simple2$merge100_pct)[[1]],
+    #merge500_pct = ks.test(factors$merge500_pct, simple2$merge500_pct)[[1]],
+    placer_pct = ks.test(factors$placer_pct, simple2$placer_pct)[[1]],
+    quartz_pct = ks.test(factors$quartz_pct, simple2$quartz_pct)[[1]],
+    recent_fires_pct = ks.test(factors$recent_fires_pct, simple2$recent_fires_pct)[[1]],
+    benchmark_pct = ks.test(factors$benchmark_pct, simple2$benchmark_pct)[[1]],
+    elev_median = ks.test(factors$elev_median, simple2$elev_median)[[1]],
+    elev_sd = ks.test(factors$elev_sd, simple2$elev_sd)[[1]],
+    forest_pct = ks.test(factors$forest_pct, simple2$forest_pct)[[1]],
+    wetland_pct = ks.test(factors$wetland_pct, simple2$wetland_pct)[[1]],
+    water_pct = ks.test(factors$water_pct, simple2$water_pct)[[1]]
   )
 
 ks_output_clust3 <- 
   tibble(
-    merge100_pct = ks.test(simple12$merge100_pct, simple3$merge100_pct)[[1]],
-    #merge500_pct = ks.test(simple12$merge500_pct, simple3$merge500_pct)[[1]],
-    placer_pct = ks.test(simple12$placer_pct, simple3$placer_pct)[[1]],
-    quartz_pct = ks.test(simple12$quartz_pct, simple3$quartz_pct)[[1]],
-    recent_fires_pct = ks.test(simple12$recent_fires_pct, simple3$recent_fires_pct)[[1]],
-    benchmark_pct = ks.test(simple12$benchmark_pct, simple3$benchmark_pct)[[1]],
-    elev_median = ks.test(simple12$elev_median, simple3$elev_median)[[1]],
-    elev_sd = ks.test(simple12$elev_sd, simple3$elev_sd)[[1]],
-    forest_pct = ks.test(simple12$forest_pct, simple3$forest_pct)[[1]],
-    wetland_pct = ks.test(simple12$wetland_pct, simple3$wetland_pct)[[1]],
-    water_pct = ks.test(simple12$water_pct, simple3$water_pct)[[1]]
+    merge100_pct = ks.test(factors$merge100_pct, simple3$merge100_pct)[[1]],
+    #merge500_pct = ks.test(factors$merge500_pct, simple3$merge500_pct)[[1]],
+    placer_pct = ks.test(factors$placer_pct, simple3$placer_pct)[[1]],
+    quartz_pct = ks.test(factors$quartz_pct, simple3$quartz_pct)[[1]],
+    recent_fires_pct = ks.test(factors$recent_fires_pct, simple3$recent_fires_pct)[[1]],
+    benchmark_pct = ks.test(factors$benchmark_pct, simple3$benchmark_pct)[[1]],
+    elev_median = ks.test(factors$elev_median, simple3$elev_median)[[1]],
+    elev_sd = ks.test(factors$elev_sd, simple3$elev_sd)[[1]],
+    forest_pct = ks.test(factors$forest_pct, simple3$forest_pct)[[1]],
+    wetland_pct = ks.test(factors$wetland_pct, simple3$wetland_pct)[[1]],
+    water_pct = ks.test(factors$water_pct, simple3$water_pct)[[1]]
   )
 
+labels <- tibble(ks_test=c('Full_sample vs All_cells', 'Clust1_sample vs All_cells', 
+    'Clust2_sample vs All_cells', 'Clust3_sample vs All_cells'))
+ks_tests <- bind_rows(ks_output, ks_output_clust1, ks_output_clust2, ks_output_clust3)
+ks_tests <- bind_cols(labels, ks_tests)
+print(ks_tests)
